a678a2cf077732c5c428f6a0973a4e8167994c19
bootstraponline <code@bootstraponline.com>
2018-07-13 10:34:56 -0400

    Fix open source. Add jib (#19)

---
+++ private-data/dataseeding/PrivateConst.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+@file:JvmName("PrivateConst")
+package com.instructure.dataseeding.util
+
+/**
+ * Changes to this file will NOT BE KEPT unless editing the original file in private-data/dataseeding!
+ */
+const val DATA_SEEDING_ADMIN_TOKEN = "a"
+const val DATA_SEEDING_CLIENT_ID = "a"
+const val DATA_SEEDING_CLIENT_SECRET = "a"
